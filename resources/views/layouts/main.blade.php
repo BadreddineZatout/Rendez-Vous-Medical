1b75b9249cc13954d03ad1957c37aa336dc656d8
Badreddinezatout <hb_zatout@esi.dz>
2020-08-29 12:09:59 +0100

    Parametres Form pour le medecin

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,7 +7,7 @@
             <div class="row">
                     <div class="col-sm-12 p-1 mb-5">
                         <img id="profilphoto" src="/images/admin.png" class="rounded-circle mx-auto d-block mb-5 mt-3" alt="Photo" width="150" height="150"> 
-                        <h3 class="text-center text-light font-weight-bolder">Bienvenu {{ Auth::user()->name }}</h1>
+                        <h3 class="text-center text-light font-weight-bolder">Bienvenu {{ Auth::user()->name ?? 'Client' }}</h1>
                     </div>
                     <div class="col-sm-12 mb-5">
                         <h1 id="option1" class="btn btn-lg btn-outline-light btn-block font-weight-bolder">Medecins</h1>
--- resources/views/medecin/home.blade.php
+++ resources/views/medecin/home.blade.php
@@ -6,4 +6,74 @@
         document.getElementById('option2').innerHTML = "Parametres"
         document.getElementById('profilphoto').src = '/images/doctor.png'
     </script>
+    <div id="parametre" class="card my-5" style="margin-left: 13em">
+        <div class="card-header bg-primary font-weight-bolder text-white">{{ __('parametres') }}</div>
+        <div class="card-body">
+            <form action="">
+                @csrf
+                
+                <div class="form-group row">
+                    <label for="etat" class="col-md-4 col-form-label text-md-right">{{__('Etat du Systeme')}}</label>
+                    <div class="col-md-6 mt-2">
+                        <input type="radio" id="oui" name="etatsysteme" value="Oui">
+                        <label class="mr-5" for="oui">Oui</label>
+                        <input type="radio" id="non" name="etatsysteme" value="Non">
+                        <label for="non">Non</label>
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="DD" class="col-md-4 col-form-label text-md-right">{{__('Date de Debut')}}</label>
+                    <div class="col-md-6">
+                        <input id="DD" type="date" class="form-control" name="DD" autocomplete="DD" autofocus>
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="DF" class="col-md-4 col-form-label text-md-right">{{__('Date de Fermeture')}}</label>
+                    <div class="col-md-6">
+                        <input id="DF" type="date" class="form-control" name="DF" autocomplete="DF" autofocus>
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="cycle" class="col-md-4 col-form-label text-md-right">{{__('Cycle de Rendez Vous')}}</label>
+                    <div class="col-md-8 mt-2">
+                        <input type="radio" id="c1" name="cyclerdv" value="15min">
+                        <label class="mr-3" for="c1">15min</label>
+                        <input type="radio" id="c2" name="cyclerdv" value="20min">
+                        <label class="mr-3" for="c2">20min</label>
+                        <input type="radio" id="c3" name="cyclerdv" value="30min">
+                        <label class="mr-3" for="c3">30min</label>
+                        <input type="radio" id="c4" name="cyclerdv" value="1h">
+                        <label class="mr-3" for="c4">1h</label>
+                        <input type="radio" id="c5" name="cyclerdv" value="2h">
+                        <label for="c5">2h</label>
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="nombre" class="col-md-4 col-form-label text-md-right">{{__('Nombre de Rendez Vous')}}</label>
+                    <div id="nombre" class="dropdown col-md-4">
+                        <button type="button" class="btn btn-outline-primary dropdown-toggle" data-toggle="dropdown">
+                          Nombre de Rendez Vous
+                        </button>
+                        <div class="dropdown-menu">
+                          <a class="dropdown-item" href="#">1</a>
+                          <a class="dropdown-item" href="#">2</a>
+                          <a class="dropdown-item" href="#">3</a>
+                        </div>
+                      </div> 
+                </div>
+
+                <div class="form-group row mb-0">
+                    <div class="col-md-6 offset-md-4">
+                        <button type="submit" class="btn btn-primary">
+                            {{ __('Sauvegarder') }}
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
 @endsection
