3d5bd3571068e716d528d5553295c6541c63f6cb
Badreddinezatout <hb_zatout@esi.dz>
2020-09-01 23:26:05 +0100

    Register Users

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Commune;
 use App\Http\Controllers\Controller;
+use App\Medecin;
+use App\Patient;
 use App\Providers\RouteServiceProvider;
+use App\Specialite;
 use App\User;
+use App\Wilaya;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
@@ -50,10 +55,13 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => ['required', 'string', 'max:255'],
+            'nom' => ['required', 'string', 'min:2:', 'max:255'],
+            'prenom' => ['required', 'string', 'min:2', 'max:255'],
+            'adr' => ['required', 'string', 'min:10', 'max:255'],
             'phone' => ['required' ,'regex:/(0)[567][0-9]{8}/', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
             'role' => ['required'],
+            'cabinet' => ['string', 'max:255'],
         ]);
     }
 
@@ -65,8 +73,32 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        $wilaya = Wilaya::select('CodeWilaya')->where('NomWilaya', $data['wilayachoice'])->get();
+        $commune = Commune::select('id')->where('NomCommune', $data['communechoice'])->get();
+        $spec = Specialite::select('id')->where('NomSpecialite', $data['specialitechoice'])->get();
+        if ($data['role'] == 'Patient'){
+            Patient::create([
+                'NomPatient' => $data['nom'],
+                'PrenomPatient' => $data['prenom'],
+                'DateNaissance' => $data['dn'],
+                'Telephone' => $data['phone'],
+                'Wilaya' => $wilaya,
+                'Commune' => $commune,
+            ]);
+        }else{
+            Medecin::create([
+                'NomMedecin' => $data['nom'],
+                'PrenomMedecin' => $data['prenom'],
+                'Adresse' => $data['adr'],
+                'Cabinet' => $data['cabinet'],
+                'Telephone' => $data['phone'],
+                'Specialite' => $spec,
+                'Wilaya' => $wilaya,
+                'Commune' => $commune,
+            ]);
+        }
         return User::create([
-            'name' => $data['name'],
+            'name' => $data['prenom'],
             'phone' => $data['phone'],
             'user_type' => $data['role'],
             'password' => Hash::make($data['password']),
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,10 +12,13 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
+
                         <div class="form-group row">
                             <label for="nom" class="col-md-4 col-form-label text-md-right">{{ __('Nom') }}</label>
+
                             <div class="col-md-6">
-                                <input id="nom" type="text" class="form-control @error('nom') is-invalid @enderror" value="{{ old('nom') }}" required autocomplete="nom">
+                                <input id="nom" type="text" class="form-control @error('nom') is-invalid @enderror" name="nom" value="{{ old('nom') }}" required autocomplete="nom" autofocus>
+
                                 @error('nom')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -26,10 +29,9 @@
 
                         <div class="form-group row">
                             <label for="prenom" class="col-md-4 col-form-label text-md-right">{{ __('Prenom') }}</label>
-
                             <div class="col-md-6">
-                                <input id="prenom" type="text" class="form-control @error('prenom') is-invalid @enderror" name="prenom" value="{{ old('prenom') }}" required autocomplete="prenom" autofocus>
-
+                                <input id="prenom" name="prenom" type="text" class="form-control @error('prenom') is-invalid @enderror" value="{{ old('prenom') }}" required autocomplete="prenom">
+                                
                                 @error('prenom')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -37,10 +39,11 @@
                                 @enderror
                             </div>
                         </div>
+
                         <div class="form-group row">
                             <label for="wilaya" class="col-md-4 col-form-label text-md-right">{{__('Wilaya')}}</label>
                             <div id="wilaya" class="dropdown col-md-4">
-                                <button id="wilayachoice" type="button" class="btn btn-outline-primary dropdown-toggle" data-toggle="dropdown">
+                                <button id="wilayachoice" name="wilayachoice" type="button" class="btn btn-outline-primary dropdown-toggle" data-toggle="dropdown">
                                     Wilaya
                                 </button>
                                 <div id="wilayalist" class="dropdown-menu overflow-auto" style="height: 10rem">
@@ -51,7 +54,7 @@
                         <div class="form-group row">
                             <label for="commune" class="col-md-4 col-form-label text-md-right">{{__('Commune')}}</label>
                             <div id="commune" class="dropdown col-md-4">
-                                <button id="communechoice" type="button" class="btn btn-outline-primary dropdown-toggle" data-toggle="dropdown">
+                                <button id="communechoice" name="communechoice" type="button" class="btn btn-outline-primary dropdown-toggle" data-toggle="dropdown">
                                     Commune
                                 </button>
                                 <div id="communelist" class="dropdown-menu overflow-auto" style="height: 10rem">
@@ -62,7 +65,7 @@
                         <div class="form-group row">
                             <label for="adr" class="col-md-4 col-form-label text-md-right">{{ __('Adresse') }}</label>
                             <div class="col-md-6">
-                                <input id="adr" type="text" class="form-control" value="{{ old('adr') }}" required>
+                                <input id="adr" name="adr" type="text" class="form-control" value="{{ old('adr') }}" required>
                             </div>
                         </div>
 
@@ -105,8 +108,8 @@
                         <div class="form-group row">
                             <label class="col-md-4 col-form-label text-md-right">{{__('Type utilisateur')}}</label>
                             <div class="col-md-6 mt-2">
-                                <input type="radio" id="admin" name="role" value="Admin">
-                                <label class="mr-3" for="admin">Admin</label>
+                                <!--<input type="radio" id="admin" name="role" value="Admin">
+                                <label class="mr-3" for="admin">Admin</label>-->
                                 <input type="radio" id="medecin" name="role" value="Medecin">
                                 <label class="mr-3" for="medecin" onclick="ToggleMedecin()">Medecin</label>
                                 <input type="radio" id="patient" name="role" value="Patient">
@@ -117,21 +120,21 @@
                         <div id="datenaissance" class="form-group row">
                             <label for="dn" class="col-md-4 col-form-label text-md-right">{{ __('Date de Naissance') }}</label>
                             <div class="col-md-6">
-                                <input id="dn" type="date" class="form-control" value="{{ old('dn') }}">
+                                <input id="dn" name="dn" type="date" class="form-control" value="{{ old('dn') }}">
                             </div>
                         </div>
 
                         <div id="cab" class="form-group row">
                             <label for="cabinet" class="col-md-4 col-form-label text-md-right">{{ __('Cabinet') }}</label>
                             <div class="col-md-6">
-                                <input id="cabinet" type="text" class="form-control" value="{{ old('cabinet') }}">
+                                <input id="cabinet" name="cabinet" type="text" class="form-control" value="{{ old('cabinet') }}">
                             </div>
                         </div>
 
                         <div id="spec" class="form-group row">
                             <label for="specialite" class="col-md-4 col-form-label text-md-right">{{__('Specialite')}}</label>
                             <div id="specialite" class="dropdown col-md-4">
-                                <button id="specialitechoice" type="button" class="btn btn-outline-primary dropdown-toggle" data-toggle="dropdown">
+                                <button id="specialitechoice" name="specialitechoice" type="button" class="btn btn-outline-primary dropdown-toggle" data-toggle="dropdown">
                                     Specialite
                                 </button>
                                 <div id="speclist" class="dropdown-menu overflow-auto" style="height: 10rem">
